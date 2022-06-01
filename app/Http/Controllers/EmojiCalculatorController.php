400d92d7dc98da549196bfe1b119f49cb65e8b84
mah.shamim <mah.shamim@gmail.com>
2022-06-02 01:24:07 +0600

    create construct method and call EmojiCalculatorService

--- app/Http/Controllers/EmojiCalculatorController.php
+++ app/Http/Controllers/EmojiCalculatorController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\EmojiCalculatorService;
 use Illuminate\Http\Request;
 
 class EmojiCalculatorController extends Controller
 {
-    //
+    private EmojiCalculatorService $emojiCalculatorService;
+
+    /**
+     * EmojiCalculatorController constructor.
+     * @param EmojiCalculatorService $emojiCalculatorService
+     */
+    public function __construct(EmojiCalculatorService $emojiCalculatorService)
+    {
+        $this->emojiCalculatorService = $emojiCalculatorService;
+    }
 }
